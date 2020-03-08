16f186f7ad1d0fab16b96f9e1d97b5fe2a02dac7
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:22 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedpersistence/btm/implementation/BTMModuleExclusions.java
+++ src/main/java/com/guicedee/guicedpersistence/btm/implementation/BTMModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedpersistence.btm.implementation;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,17 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BTMModuleExclusions
-		implements IGuiceScanJarExclusions<BTMModuleExclusions>,
-				           IGuiceScanModuleExclusions<BTMModuleExclusions>
+		implements IGuiceScanModuleExclusions<BTMModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("btm-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
