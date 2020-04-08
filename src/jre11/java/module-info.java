60cbf9d05df27878cc7d93b61fb156d0f92cd667
GedMarc <marc.magon@gmail.com>
2020-04-08 22:10:46 +0200

    Sanitizing module-info.java

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,14 +3,12 @@ module com.guicedee.guicedpersistence.btm {
 	exports com.guicedee.guicedpersistence.btm;
 
 	requires transitive com.guicedee.guicedpersistence;
-	requires tm.bitronix.btm;
+	requires transitive tm.bitronix.btm;
 
 	requires transitive java.sql;
-
-	requires java.validation;
 	requires java.naming;
 
-	requires com.guicedee.guicedpersistence.readers.hibernateproperties;
+	requires transitive com.guicedee.guicedpersistence.readers.hibernateproperties;
 
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.btm.implementation.BTMConnectionProperties;
 	provides com.guicedee.guicedpersistence.services.ITransactionHandler with com.guicedee.guicedpersistence.btm.implementation.BTMAutomatedTransactionHandler;
